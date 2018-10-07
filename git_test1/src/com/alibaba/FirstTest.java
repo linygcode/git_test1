7c3f81842c8956344ac79cd4cce662c4ffa530a7
2127 <admin@163.com>
2018-10-07 16:00:02 +0800

    把本地工程添加到git

---
+++ git_test1/src/com/alibaba/FirstTest.java
@@ -0,0 +1,13 @@
+package com.alibaba;
+
+public class FirstTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+
+    }
+}
